7810ffb0a8b731347893cddfcdbc4c76566e4060
vinayaksgit <svinayak.30@gmail.com>
2025-01-15 03:31:09 +0530

    mycode

---
+++ src/main/java/com/example/makemytrip_ms/MakemytripMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.makemytrip_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakemytripMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakemytripMsApplication.class, args);
+	}
+
+}
